d9b8acebe47bd9abcc9ef1f6ab50a72a612b2632
Neil Deshmukh <neil.nitin.de@Gmail.com>
2020-11-08 01:27:35 -0500

    Update estimate_nuisances.R

--- R/estimate_nuisances.R
+++ R/estimate_nuisances.R
@@ -52,7 +52,7 @@
             ret$prop.fit <- list(whichScreen = whichScreen, pred.alg = prop.fit)
         }
     } else {
-        prop.fit <- SuperLearner(Y=A, X=W, newX=newW, family=binomial(),
+        prop.fit <- SuperLearner(Y=A, X=W, newX=newW, family="binomial",
                                  SL.library=SL.library, method = "method.NNloglik", verbose = verbose)
         ret$prop.pred <- c(prop.fit$SL.predict)
         if(save.fit) ret$prop.fit <- prop.fit
